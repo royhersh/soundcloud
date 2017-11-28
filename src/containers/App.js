5099d781054b40329607396a780edc812477e2a7
Roy Hershkovitz <roy.hershkovitz@mce-sys.com>
2017-11-28 11:33:37 +0200

    search container wireframing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import SC from 'soundcloud';
 /* CSS */
 import '../style/app.scss';
 import '../style/font-awesome/css/font-awesome.min.css';
@@ -8,20 +8,57 @@ import '../style/font-awesome/css/font-awesome.min.css';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+
+		SC.initialize({
+			client_id: 'ggX0UomnLs0VmW7qZnCzw'
+		});
 	}
 
 	render() {
-	
+
+		// find all sounds of buskers licensed under 'creative commons share alike'
+		SC.get('/tracks', {
+			q: 'pixies',
+			limit: 6,
+			linked_partitioning: 2
+		}).then(function (tracks) {
+			console.log(tracks);
+		});
+
 		return (
-			<h1>Hello from React</h1>
+			<div className="appContainer">
+
+				<div className="search container">
+					<div className="inputBox">
+						<input type="text" />
+						<button>Search</button>
+					</div>
+
+					<ul className="tracksList">
+						<li><a href="#">Zurich</a></li>
+						<li><a href="#">Geneva</a></li>
+						<li><a href="#">Winterthur</a></li>
+						<li><a href="#">Lausanne</a></li>
+						<li><a href="#">Lucerne</a></li>
+						<li><a href="#">Alabamba</a></li>
+					</ul>
+					<div className="footer">
+						<button><i className="fa fa-long-arrow-left" aria-hidden="true"></i></button>
+						<div className="gap"></div>
+						<i className="fa fa-list fa-6" aria-hidden="true"></i>
+						<i className="fa fa-th-large fa-2" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div className="image container"></div>
+				<div className="history container"></div>
+			</div>
 		);
-      
+
 	}
 }
 
-const mapStateToProps = (state) => ({
-	user: state.auth.user
-});
+
 
 export default App;
 
